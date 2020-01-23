aa8999f3b00fb7d091509fc26e98e412471364d0
lucaslima18 <lucas.ala1999@gmail.com>
2020-01-23 14:54:58 -0300

    insert tests function

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ from settings import *
 from request import *
 
 
-file_csv = open('spreadsheets/inicial.csv')
+file_csv = open("spreadsheets/inicial.csv")
+
 
 def insertExamples():
-    print("starting the post test on your bothub repository...\n")
+    print("starting the post examples on your bothub repository...\n")
     for message in csv.DictReader(file_csv):
-        
+
         text = message["text"]
         intent = message["intent"]
         postExample(text, intent)
         print(message["text"])
 
+
 def insertTests():
-    pass
+    print("starting the post tests on your bothub repository...\n")
+    for message in csv.DictReader(file_csv):
+
+        text = message["text"]
+        intent = message["intent"]
+        postTests(text, intent)
+        print(message["text"])
 
 def categorize():
     pass
 
+
 print("#####WELCOME TO AI SPREADSHEET CONVERTER#####")
 
-if SERVICE_TYPE == 'test':
-    print("starting the post test on your bothub repository...\n")
+if SERVICE_TYPE == "tests":
     insertTests()
 
-elif SERVICE_TYPE == 'categorize':
+elif SERVICE_TYPE == "categorize":
     print("")
     categorize()
 
-elif SERVICE_TYPE == 'examples':
+elif SERVICE_TYPE == "examples":
     print()
     insertExamples()
--- request.py
+++ request.py
@@ -17,4 +17,21 @@ def postExample(text, intent):
             }
     print(f"{requests.post(url, data=json.dumps(data), headers=headers)}\n")
 
+def postTests(text, intent):
+    url = "https://api.bothub.it/v2/repository/evaluate/"
+    data = {
+            "repository": REPOSITORY_UUID,
+            "text": text,
+            "intent": intent,
+            "language": LANGUAGE,
+            "entities": []
+        }
+    headers = {
+                'Content-Type': 'application/json',
+                'Authorization': AUTH_TOKEN
+            }
+    print(f"{requests.post(url, data=json.dumps(data), headers=headers)}\n")
+
+
+
 
